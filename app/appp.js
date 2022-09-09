57439527febfcc2cb0b68e7c255f257863c67fab
RAHAT HASAN DIPO <rahathasandipto@gmail.com>
2022-09-09 20:23:23 +0600

    This is the last commit

--- app/appp.js
+++ app/appp.js
@@ -1,11 +1,16 @@
 document.getElementById('btn-sumbit').addEventListener('click', function () {
     const inputFiled = document.getElementById('input-filed');
     const inputFiledValue = inputFiled.value;
+    const inputPass = document.getElementById('input-pass');
+    const inputPassValue = inputPass.value;
     inputFiled.value = '';
-    if (inputFiledValue === '111') {
-        // pageRedirect();
-        window.location.href = "https://www.tutorialrepublic.com/";
-    } else {
-        'ýour passoword is  incorrect'
+    if (inputPassValue === 'ranaahmed@gmail.com' && inputFiledValue === '111') {
+        // This is the link of html page
+        window.location.href = "new.html";
     }
+    else if (inputFiledValue !== '111') {
+        // show the alert 
+        alert('Your passoword is  incorrect')
+    }
+
 })
